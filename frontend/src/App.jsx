826b6232b2b3272616b1807a59337d27b3d08e7a
leontoys <svliyon@gmail.com>
2025-03-04 11:46:08 +1100

    loading-status-added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,10 +6,12 @@ import './App.css';
 function App() {
 
   const [movies,setMovies] = useState([])
+  const [loading,setLoading] = useState(false) //for loading status
  
   const onSearch = async (query) => {
     if(!query) return
 
+    setLoading(true) //we are querying - starts loading
     const apiKey = import.meta.env.VITE_TMDB_API_KEY
     const url = `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&language=en-US&query=${query}&page=1&include_adult=false`
     try {
@@ -21,6 +23,9 @@ function App() {
     } catch (error) {
         console.error('Error fetchning movies:',error)
     }
+    finally{
+      setLoading(false)
+    }
 
 
   };
@@ -29,7 +34,13 @@ function App() {
     <div className="App">
       <h1>Movie Search App</h1>
       <SearchForm onSearch={onSearch} />
-      <MovieList movies={movies}/>
+      {
+        loading ? 
+        ( <p>Loading ...</p> ) 
+        :(
+          <MovieList movies={movies}/>
+        )
+      }
     </div>
   );
 }
